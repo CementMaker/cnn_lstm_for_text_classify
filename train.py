f4ac0b055a955d998237b55a9446d7e5086bc52e
CementMaker <tangzhoulin@gmail.com>
2019-09-11 00:35:03 +0800

    add dynamic rnn

--- train.py
+++ train.py
@@ -12,9 +12,10 @@ from sklearn.metrics import classification_report
 
 
 # mini batch每个batch迭代
-def get_batch(epoches, batch_size):
-    train_x, train_y = pickle.load(open("./data/pkl/train.pkl", "rb"))
-    data = list(zip(train_x, train_y))
+def get_batch(epoches, batch_size, train_x=None, train_y=None, seq_length=None):
+    if train_x is None:
+        train_x, train_y = pickle.load(open("./data/pkl/train.pkl", "rb"))
+    data = list(zip(train_x, train_y, seq_length))
 
     for epoch in range(epoches):
         random.shuffle(data)
@@ -54,7 +55,7 @@ def dev_step(model_train, batch, label, return_predict=False):
     
     # 写入tensorBoard
     model_train.summary_writer_test.add_summary(summary, step)
-    print("test: step {}, loss {}, accuracy {}".format(step, loss, accuracy))
+    print("\t test: step {}, loss {}, accuracy {}".format(step, loss, accuracy))
 
 
 class NeuralBowTrain(object):
@@ -115,7 +116,7 @@ class textRnnTrain(object):
                                    rnn_size=100,
                                    num_classes=6)
         self.global_step = tf.Variable(0, name="global_step", trainable=False)
-         
+
         # 定义optimizer
         self.optimizer = tf.train.AdamOptimizer(0.005).minimize(self.model.loss, global_step=self.global_step)
         self.sess.run(tf.global_variables_initializer())
@@ -130,14 +131,45 @@ class textRnnTrain(object):
         self.summary_writer_test = tf.summary.FileWriter("./summary/rnn_summary/test", graph=self.sess.graph)
 
 
+class textDynamicRnnTrain(object):
+    def __init__(self):
+        self.feature = DynamicRnnfeature()
+        
+        self.sess = tf.Session()
+        self.model = TextDynamicRNN.lstm(layer_sizes=[128, 256],
+                                         sequence_length=,
+                                         embedding_size=100,
+                                         vocab_size=20005,
+                                         rnn_size=100,
+                                         num_classes=6,
+                                         max_length=3236)
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
+
+        # 定义optimizer
+        self.optimizer = tf.train.AdamOptimizer(0.005).minimize(self.model.loss, global_step=self.global_step)
+        self.sess.run(tf.global_variables_initializer())
+        self.batches = get_batch(1, 400, self.feature.train_x, self.feature.train_y)
+
+        # tensorBoard
+        tf.summary.scalar('loss', self.model.loss)
+        tf.summary.scalar('accuracy', self.model.accuracy)
+        self.merged_summary_train = tf.summary.merge_all()
+        self.merged_summary_test = tf.summary.merge_all()
+        self.summary_writer_train = tf.summary.FileWriter("./summary/dynamic_rnn_summary/train", graph=self.sess.graph)
+        self.summary_writer_test = tf.summary.FileWriter("./summary/dynamic_rnn_summary/test", graph=self.sess.graph)
+
+
 def main(model_train):
-    test_x, test_y = pickle.load(open("./data/pkl/test.pkl", "rb"))
+    try:
+        test_x, test_y = model_train.feature.test_x, model_train.feature.test_y
+    except:
+        test_x, test_y = pickle.load(open("./data/pkl/test.pkl", "rb"))
     for data in model_train.batches:
         x_train, y_train = zip(*data)
         train_step(model_train, x_train, y_train)
         current_step = tf.train.global_step(model_train.sess, model_train.global_step)
         if current_step % 10 == 0:
-            print("\n:")
+            print("dev step\t:")
             dev_step(model_train, test_x, test_y)
 
     dev_step(model_train, test_x, test_y, return_predict=True)
@@ -147,6 +179,7 @@ def main(model_train):
 if __name__ == "__main__":
     # Net = NeuralBowTrain()
     # Net = textCnnTrain()
-    Net = textRnnTrain()
+    # Net = textRnnTrain()
+    Net = textDynamicRnnTrain()
     main(Net)
 
